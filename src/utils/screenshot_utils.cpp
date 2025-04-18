dbe9e8b67383dc2fec10a6dae4fbd88a5c8363e9
NelsonBlakeN <nelson.blaken@gmail.com>
2025-04-18 09:08:48 -0500

    Use preprocessing where necessary, such as class determination

--- src/utils/screenshot_utils.cpp
+++ src/utils/screenshot_utils.cpp
@@ -10,8 +10,9 @@
 #include <tesseract/baseapi.h>
 #include <leptonica/allheaders.h>
 #include <QCryptographicHash>
-
-const bool saveImage = true;
+#include "file_utils.hpp"
+#include "string_utils.hpp"
+#include <tesseract/publictypes.h>
 
 double calculateMSE(const QImage &img1, const QImage &img2)
 {
@@ -43,7 +44,32 @@ double calculateMSE(const QImage &img1, const QImage &img2)
     return mse;
 }
 
-std::string grabAndProcessArea(std::tuple<int, int, int, int> area, QScreen *screen, std::string imageName)
+Pix *BinarizeImage(Pix *pixImage)
+{
+    // Convert image to grayscale
+    Pix *grayImage = pixConvertRGBToLuminance(pixImage);
+
+    // // Apply threshold to binary
+    // Pix *binaryImage = pixThresholdToBinary(grayImage, 128);
+
+    // Good threshold for star recognition: 168 (although not mandatory)
+    Pix *binaryImage = pixThresholdToBinary(grayImage, 200);
+
+    pixDestroy(&grayImage); // Clean up the grayscale image
+    return binaryImage;
+}
+
+void SavePixToFile(Pix *pix, const char *filename)
+{
+    pixWrite(filename, pix, IFF_PNG);
+}
+
+std::string grabAndProcessArea(
+    std::tuple<int, int, int, int> area,
+    QScreen *screen,
+    std::string imageName,
+    bool saveImage,
+    tesseract::PageSegMode psm)
 {
     QRect captureArea(std::get<0>(area), std::get<1>(area), std::get<2>(area), std::get<3>(area));
     QPixmap screenshot = screen->grabWindow(0, captureArea.x(), captureArea.y(), captureArea.width(), captureArea.height());
@@ -76,6 +102,31 @@ std::string grabAndProcessArea(std::tuple<int, int, int, int> area, QScreen *scr
         return "";
     }
 
+    SavePixToFile(pixImage, "original_image.png");
+    Pix *binaryImage = BinarizeImage(pixImage);
+
+    if (saveImage)
+    {
+        SavePixToFile(binaryImage, "binary_image.png");
+
+        api.SetPageSegMode(psm);
+        api.SetImage(binaryImage);
+        char *binaryText = api.GetUTF8Text();
+        if (binaryText)
+        {
+            std::string binaryResult(binaryText);
+            std::replace(binaryResult.begin(), binaryResult.end(), '\n', ' ');
+            delete[] binaryText;
+            binaryResult = rtrim(binaryResult);
+
+            if (imageName.find("class_") != std::string::npos)
+            {
+                return binaryResult;
+            }
+        }
+    }
+
+    api.SetPageSegMode(psm);
     api.SetImage(pixImage);
 
     char *text = api.GetUTF8Text();
